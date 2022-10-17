281f96c9b920ee6cb1dc2b515d50919a6ca88ad9
Agustin Farisano <farisanoagustin@gmail.com>
2022-10-17 08:54:00 -0300

    detect input value with viewchild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -10,6 +10,15 @@ export class BusquedaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = ' ';
   }
 
 }
