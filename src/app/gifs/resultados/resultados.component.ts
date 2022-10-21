059347f2eef9ea62bfdf087f90b5ae566baeabb1
Agustin Farisano <farisanoagustin@gmail.com>
2022-10-21 13:45:03 -0300

    add an http get to the API for show a query in a results html view component

--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadosComponent implements OnInit {
 
-  constructor() { }
+  get resultados(){
+    return this.GifsService.resultados;
+  }
+
+  constructor(private GifsService:GifsService) { }
 
   ngOnInit(): void {
   }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,22 @@
 import { query } from '@angular/animations';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscriber } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey:string = 'tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V';
   private _historial:string[] = [];
+  public resultados:any[]=[];
   
   get historial(){
     return [...this._historial];
   }
+
+  constructor(private http:HttpClient) { }
 
   buscarGifs(query:string){
 
@@ -20,9 +26,13 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial =  this._historial.splice(0,10);
     }
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V&q=${query} z&limit=10`)
+      .subscribe((resp:any) => {
+         console.log(resp.data);
+         this.resultados = resp.data;
+      })
     
     console.log(this._historial);
   }
-
-  constructor() { }
 }
