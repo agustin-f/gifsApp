86a93b1b10e1488d417b3155390498e610b7a917
Agustin Farisano <farisanoagustin@gmail.com>
2022-11-02 16:00:18 -0300

    add gif data interface model into a service and load a historic info with a localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -2,6 +2,7 @@ import { query } from '@angular/animations';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscriber } from 'rxjs';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,18 @@ export class GifsService {
 
   private apiKey:string = 'tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V';
   private _historial:string[] = [];
-  public resultados:any[]=[];
+  public resultados:Gif[]=[];
   
   get historial(){
     return [...this._historial];
   }
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    // if(localStorage.getItem('historial')){
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+   }
 
   buscarGifs(query:string){
 
@@ -25,9 +31,11 @@ export class GifsService {
     if( !this._historial.includes(query) ){
       this._historial.unshift(query);
       this._historial =  this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V&q=${query} z&limit=10`)
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V&q=${query} z&limit=10`)
       .subscribe((resp:any) => {
          console.log(resp.data);
          this.resultados = resp.data;
