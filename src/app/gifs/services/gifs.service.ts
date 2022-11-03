b9b3bd5664bc620cd86e74a7f9d5c3a941dc8332
Agustin Farisano <farisanoagustin@gmail.com>
2022-11-03 09:22:49 -0300

    add api url params

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,6 @@
 import { query } from '@angular/animations';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Subscriber } from 'rxjs';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
@@ -10,6 +10,7 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 export class GifsService {
 
   private apiKey:string = 'tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V';
+  private servicioUrl:string = 'https://api.giphy.com/v1/gifs';
   private _historial:string[] = [];
   public resultados:Gif[]=[];
   
@@ -37,7 +38,15 @@ export class GifsService {
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=tGnD40JxVIw33QlwnY8rnX6Tu9OAE50V&q=${query} z&limit=10`)
+
+    const params = new HttpParams()
+        .set('api_key',this.apiKey)
+        .set('limit', '10')
+        .set('q',query)
+
+        
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
       .subscribe((resp:any) => {
          console.log(resp.data);
          this.resultados = resp.data;
